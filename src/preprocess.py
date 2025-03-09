b61173e9eb90dec8f9b6dd52bd705e8b824fd683
Lampo10 <luis_soto_uc@hotmail.com>
2025-03-09 08:54:34 +0100

    🎨 update to the preprocess and view

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import numpy as np
 
 def cambio_escala(df):
+    """"
+    Transformacion lineal de 1 al 7 a 0 al 10
+    """
     df["score"] = pd.to_numeric(df["score"], errors="coerce")
-    df['score'] = df['score'].apply(lambda x: 1 + (x -1) * 9 /6 if pd.notnull(x) else x)
+    df['score'] = df['score'].apply(lambda x: np.round((x -1) * 10 /6, 2) if pd.notnull(x) else x)
     return df
